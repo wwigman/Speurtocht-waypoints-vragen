4371563482f4674e694e463b41618768784cb659
wwigman <ww@w1gman.nl>
2026-09-04 12:37:51 +0200

    Initial commit: speurtocht PWA

---
+++ speurtocht/sw.js
@@ -0,0 +1,14 @@
+// Service worker: caches the app shell so it works without network.
+const CACHE = 'speurtocht-v2';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
+});
